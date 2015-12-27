f609b08d846e28de08e6ed41f48aeec3eace6d7f
Zachary West <zacwest@gmail.com>
2015-12-27 15:21:08 -0800

    bouncey bounce - avoid layout

--- Example/ZSWSuffixTextView/ZSWViewController.m
+++ Example/ZSWSuffixTextView/ZSWViewController.m
@@ -21,6 +21,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    self.suffixTextView.alwaysBounceVertical = YES;
     self.suffixTextView.placeholder = @"Please enter sandwich";
 }
 
